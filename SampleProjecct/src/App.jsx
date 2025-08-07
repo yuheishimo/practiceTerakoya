5975424386db76228e6f650ecbd8cd14c73eafd0
yuhei <yuu11s10o@gmail.com>
2025-08-07 10:11:11 +0900

    20250807

--- SampleProjecct/src/App.jsx
+++ SampleProjecct/src/App.jsx
@@ -1,35 +1,31 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
+// コンポーネント
 function App() {
+  //定義
   const [count, setCount] = useState(0);
+  // 奇数・偶数判定
+  const isEven = count % 2 === 0;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Hello!! Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="container">
+      <div className="counter-box">
+        <h1>カウンター</h1>
+        <p>現在のカウント：{count}</p>
+        {/* isEvenがtrueだと偶数 */}
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          これは <strong>{isEven ? '偶数' : '奇数'}</strong> です
         </p>
+        <div className="button-group">
+          {/* カウントを1減らす */}
+          <button onClick={() => setCount(count - 1)}>-</button>
+          {/* カウントを1増やす */}
+          <button onClick={() => setCount(count + 1)}>+</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
-
+// 他のファイルで使用する処理
 export default App;
